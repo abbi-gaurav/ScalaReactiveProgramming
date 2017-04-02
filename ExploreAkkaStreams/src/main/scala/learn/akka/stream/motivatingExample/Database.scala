e09cbd2a98d046f315791bb71afecbddbaf34b5a
abbi-gaurav <abbi.gaurav@gmail.com>
2017-04-02 17:39:28 +0200

    Motivating Example - 02: Optimize with buffering

--- ExploreAkkaStreams/src/main/scala/learn/akka/stream/motivatingExample/Database.scala
+++ ExploreAkkaStreams/src/main/scala/learn/akka/stream/motivatingExample/Database.scala
@@ -4,6 +4,8 @@ package learn.akka.stream.motivatingExample
   * Created by gabbi on 02.04.17.
   */
 class Database() {
+  def bulkInsertAsync(messages: Seq[String]) = println(s"inserting bulk ${messages.size} messages")
+
   def insertAsync(message: String): Unit = println(s"persisting message $message")
 
 }
--- ExploreAkkaStreams/src/main/scala/learn/akka/stream/motivatingExample/DatabaseActor.scala
+++ ExploreAkkaStreams/src/main/scala/learn/akka/stream/motivatingExample/DatabaseActor.scala
@@ -7,15 +7,31 @@ import learn.akka.stream.motivatingExample.DatabaseActor.InsertMessage
 /**
   * Created by gabbi on 02.04.17.
   */
-class DatabaseActor extends Actor{
+class DatabaseActor extends Actor {
   private val database = new Database()
+  var messages: Seq[String] = Nil
+  var count: Int = 0
+
+
   override def receive: Receive = {
-    case InsertMessage(message) => database.insertAsync(message)
+    case InsertMessage(message) =>
+      messages = message +: messages
+      count += 1
+      if (count == 5) {
+        insert()
+      }
+  }
+
+  private def insert(): Unit = {
+    database.bulkInsertAsync(messages)
+    messages = Nil
+    count = 0
   }
 }
 
-object DatabaseActor{
-  case class InsertMessage(message:String)
+object DatabaseActor {
+
+  case class InsertMessage(message: String)
 
   object InsertMessage {
     def ack(message: InsertMessage): Message = TextMessage("ack")
