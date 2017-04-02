9cdb00aa11a1446111b28d146616400076a45dbe
abbi-gaurav <abbi.gaurav@gmail.com>
2017-04-02 17:48:32 +0200

    Motivating Example - 03: Optimize with scheduling

--- ExploreAkkaStreams/src/main/scala/learn/akka/stream/motivatingExample/DatabaseActor.scala
+++ ExploreAkkaStreams/src/main/scala/learn/akka/stream/motivatingExample/DatabaseActor.scala
@@ -2,16 +2,22 @@ package learn.akka.stream.motivatingExample
 
 import akka.actor.Actor
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import learn.akka.stream.motivatingExample.DatabaseActor.InsertMessage
+import learn.akka.stream.motivatingExample.DatabaseActor.{Insert, InsertMessage}
+
+import scala.concurrent.duration._
 
 /**
   * Created by gabbi on 02.04.17.
   */
 class DatabaseActor extends Actor {
   private val database = new Database()
-  var messages: Seq[String] = Nil
-  var count: Int = 0
+  private var messages: Seq[String] = Nil
+  private var count: Int = 0
+  private implicit val ec = context.dispatcher
 
+  override def preStart(): Unit = context.system.scheduler.scheduleOnce(1 second) {
+    self ! Insert
+  }
 
   override def receive: Receive = {
     case InsertMessage(message) =>
@@ -20,12 +26,20 @@ class DatabaseActor extends Actor {
       if (count == 5) {
         insert()
       }
+
+    case Insert =>
+      insert()
+      context.system.scheduler.scheduleOnce(1 second) {
+        self ! Insert
+      }
   }
 
   private def insert(): Unit = {
-    database.bulkInsertAsync(messages)
-    messages = Nil
-    count = 0
+    if (count > 0) {
+      database.bulkInsertAsync(messages)
+      messages = Nil
+      count = 0
+    }
   }
 }
 
@@ -38,5 +52,7 @@ object DatabaseActor {
 
     def parse(text: String): InsertMessage = new InsertMessage(text)
   }
+
+  case object Insert
 
 }
